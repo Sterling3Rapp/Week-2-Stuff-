08ffec8ce65cebf994452a460a9d94bdfc7bbf8c
Sterling Rapposelli III <rapp7889@chawk.cecil.edu>
2017-09-07 12:24:40 -0400

    This is what we did in Week 2

---
+++ Phone Text.py
@@ -0,0 +1,11 @@
+s = input("Enter a string to test: ")
+
+if len(s) != 12:
+    print("Not a Phone!")
+
+elif not s[0:3].isdecimal():
+    print("Not a Phone!")
+
+elif s[3] == '-' and not s[7] == '-':
+    print ("Not a Phone!")
+
